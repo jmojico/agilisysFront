e81ef8a3ecd063b175722ea3fb3ff823753786ad
Julian.Mojico <jmojico@gmail.com>
2017-05-09 16:45:37 -0300

    Cambios en seccion

--- email.php
+++ email.php
@@ -2,7 +2,8 @@
 $to      = 'jmojico@gmail.com';
 $subject = "Agilisys - Contacto de cliente >> " . $_POST["subject"];
 $message = "Mensaje de " . $_POST['name'] . ":\r\n " . $_POST['message'];
-$headers = 'From: '. $_POST['email'] . "\r\n" .
+//$headers = 'From: '. $_POST['email'] . "\r\n" .
+$headers = 'From: '. 'contacto@agilisys.com.ar' . "\r\n" .
     'Reply-To: '. $_POST['email'] . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
